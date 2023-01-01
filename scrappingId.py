551316a8fa6d409996013cfe5e656730500b732c
johan1103 <johan1103@naver.com>
2023-01-01 21:39:49 +0900

    Add scrappingId

---
+++ scrappingId.py
@@ -0,0 +1,109 @@
+import time
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def scrappingCity(cityname,driver,restaurants,idset,duplicatedlist):
+    url = 'https://www.diningcode.com//list.dc?query='+cityname
+    driver.get(url)
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "PoiBlockContainer"))
+        )  # 입력창이 뜰 때까지 대기
+    finally:
+        pass
+
+    beforeidx = 0
+    nowidx = 0
+    while True:
+        restaurantblocks = driver.find_elements(By.CSS_SELECTOR, ".PoiList > ol > li")
+        nowidx = len(restaurantblocks)
+        if nowidx == beforeidx:
+            break
+        driver.execute_script("arguments[0].scrollIntoView(true);", restaurantblocks[len(restaurantblocks) - 1])
+        time.sleep(3)
+        beforeidx = nowidx
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    content = soup.find_all(class_='PoiBlockContainer')
+    for restaurantinfo in content:
+        restaurantfullid = restaurantinfo.select_one('.PoiBlock')['id']
+        restaurantid = restaurantfullid[5:]
+        restaurantname = restaurantinfo.select_one('.InfoHeader > h2').string
+
+        beforesetlen = len(idset)
+        idset.add(restaurantid)
+        aftersetlen = len(idset)
+        if aftersetlen != beforesetlen:
+            restaurants.append({'id': restaurantid, 'name': restaurantname})
+        else:
+            duplicatedlist.append({'id': restaurantid, 'name': restaurantname})
+
+def diningcodeScrapping():
+    print('hello world')
+    driver = webdriver.Chrome('chromedriver')
+    citylist=['신사동','압구정동','논현동','청담동','삼성동','대치동','역삼동','도곡동','개포동',
+              '일원동','수서동','세곡동','자곡동','율현동',
+              '강일동','상일동','고덕동','명일동','암사동','천호동','성내동','길동','둔촌동','미아동',
+              '번동','수유동','우이동',
+              '염창동','등촌동','화곡동','내발산동','가양동','마곡동','외발산동','공항동','과해동','오곡동',
+              '오쇠동','방화동','개화동',
+              '봉천동','남현동','신림동',
+              '중곡동','능동','구의동','광장동','자양동','화양동','군자동',
+              '신도림동','구로동','가리봉동','고척동','개봉동','오류동','천왕동','항동','온수동','궁동',
+              #금천구부터
+              '가산동','독산동','시흥동','월계동','공릉동','하계동','중계동','상계동',
+              '쌍문동','방학동','창동','도봉동',
+              '신설동','제기동','전농동','답십리동','장안동','청량리동','회기동','휘경동','이문동',
+              '노량진동','상도동','흑석동','사당동','대방동','신대방동',
+              '아현동','도화동','용강동','대흥동','염리동','신수동','창전동','서교동','합정동','망원동','연남동','성산동'
+              ,'상암동',
+              '천연동','북아현동','대신동','연희동','홍제동','홍은동','남가좌동','북가좌동',
+              '서초동','잠원동','반포동','방배동','양재동','내곡동',
+              '하왕십리동','상왕십리동','마장동','사근동','행당동','응봉동','금호동1가','금호동2가','금호동3가','금호동4가'
+              ,'옥수동','성수동1가','성수동2가','서울송정동','용답동',
+              '성북동','삼선동','동선동','돈암동','안암동','보문동','정릉동','길음동','종암동','하월곡동','장위동','석관동',
+              '풍납동','거여동','마천동','방이동','오금동','송파동','석촌동','삼전동','가락동','문정동','장지동','잠실동','신천동',
+              '목동','신월동','신정동','영등포동','여의도동','당산동1가','당산동2가','당산동3가','당산동4가','당산동5가','당산동6가'
+              ,'도림동','문래동1가','문래동2가','문래동3가','문래동4가','문래동5가','문래동6가','양평동1가','양평동2가','양평동3가'
+              ,'양평동4가','양평동5가','양평동6가','신길동','대림동',
+              '후암동','용산동1가','용산동2가','용산동3가','청파동1가','청파동2가','원효로1가','원효로2가','원효로3가','효창동'
+              ,'용문동','한강로1가','한강로2가','한강로3가','이촌동','이태원동','한남동','서빙고동','보광동',
+              '녹번동','불광동','갈현동','구산동','대조동','응암동','역촌동','신사동','증산동','수색동','진관동',
+              '청운동','사직동','삼청동','부암동','평창동','무악동','교남동','가회동','종로1가','종로2가','종로3가','종로4가','종로5가'
+              ,'종로6가','이화동','혜화동','창신동','숭인동',
+              '소공동','화현동1가','태평로2가','남대문로1가','남대문로2가','남대문로3가','남대문로4가','남대문로5가'
+              ,'회현동','명동','필동','장충동','광희동','을지로동','신당동','다산동','약수동','청구동','신당5동','동화동'
+              ,'황학동','중림동',
+              '면목동','상봉동','중화동','묵동','망우동','신내동']
+    #citylist=['신사동','압구정동']
+    restaurants = []
+    duplicatedlist = []
+    idset = set()
+    for cityname in citylist:
+        scrappingCity(cityname,driver,restaurants,idset,duplicatedlist)
+
+    f = open("scrap.txt", 'w')
+    for restaurant in restaurants:
+        f.write('id ')
+        f.write(restaurant['id'])
+        f.write(' name ')
+        f.write(restaurant['name'])
+        f.write('\n')
+    f.close()
+    f = open("duplicated.txt",'w')
+    for duplicated in duplicatedlist:
+        f.write('id ')
+        f.write(duplicated['id'])
+        f.write(' name ')
+        f.write(duplicated['name'])
+        f.write('\n')
+    f.close()
+    driver.quit()
+
+if __name__ == '__main__':
+    diningcodeScrapping()
+
